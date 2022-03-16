c6be90f1d816c6e1974cbce194f18ec421520215
learnerzhang <learnerzhang@163.com>
2022-03-16 11:10:12 +0800

    init

--- vfaiss/faiss_efficiency.py
+++ vfaiss/faiss_efficiency.py
@@ -33,16 +33,18 @@ def build_faiss(dim=1000, nlist = 100):
     queryDat = genQueryDat(dim=dim)
     print('创建索引...')
     start = time.time()
-    index = faiss.IndexFlatL2(dim)      # build the index
-
+    # index = faiss.IndexFlatL2(dim)      # build the index
     quantizer = faiss.IndexFlatL2(dim)  # the other index
     # faiss.METRIC_L2: faiss定义了两种衡量相似度的方法(metrics)，
     # 分别为faiss.METRIC_L2 欧式距离、 faiss.METRIC_INNER_PRODUCT 向量内积
     # here we specify METRIC_L2, by default it performs inner-product search
     index = faiss.IndexIVFFlat(quantizer, dim, nlist, faiss.METRIC_L2)
-    print(index.is_trained)
     index.add(trainDat)                 # add vectors to the index
     print(index.ntotal)
+    assert not index.is_trained
+    index.train(trainDat)
+    assert index.is_trained
+    print(index.ntotal)  # 加入了多少行数据
     end = time.time()
     total_time = (end - start)*1000
     print('用时:%4f 毫秒' % total_time )
